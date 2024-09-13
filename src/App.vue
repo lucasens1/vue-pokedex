886c4d7cabc9428028c42d463f94432c86218f12
lucasens1 <lucasensini1@gmail.com>
2024-09-13 13:27:48 +0200

    lil fixes

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,7 @@ export default {
       intervalId: null,
     };
   },
-  computed : {
-  },
+  computed: {},
   methods: {
     handlePokemonInfo(data) {
       this.pokemonInfo = data;
@@ -34,9 +33,12 @@ export default {
         }
       }, 1500);
     },
-    statBar(statVal){
+    statBar(statVal) {
       const maxStat = 100;
-      return (statVal / maxStat) * 100 + '%';
+      return (statVal / maxStat) * 100 + "%";
+    },
+    firstL(word){
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
     }
   },
   beforeDestroy() {
@@ -48,41 +50,35 @@ export default {
 </script>
 
 <template>
-  <div class="container" style="width: 100%">
-    <div
-      class="ms_pokedex"
-      style="
-        margin: 0 auto;
-        background-color: aliceblue;
-        width: 75%;
-        height: 700px;
-        display: flex;
-      "
-    >
-      <div
-        class="ms_pokedex-l"
-        style="
-          width: 50%;
-          background-color: darkred;
-          color: white;
-          display: flex;
-          flex-direction: column;
-        "
-      >
+  <div class="container">
+    <div class="ms_pokedex">
+      <div class="ms_pokedex-l">
         <SearchCustom @pokemon-info="handlePokemonInfo" />
 
-        <div class="ms_poke-image" style="display: flex; align-items: center; justify-content: center; background-color: white;">
-          <img :src="currentSprite" alt="Immagine Pokemon" v-if="pokemonInfo">
-          <img src="../src/assets/pixel-pokeball-pixel-art-pokemon-pokeball-nintendo-8bit_grande.webp" alt="Placeholder Image" style="width: 100%;" v-else>
+        <div class="ms_poke-image">
+          <div class="ms_poke-image-rel">
+            <img
+              :src="currentSprite"
+              alt="Immagine Pokemon"
+              v-if="pokemonInfo"
+            />
+            <img
+              src="../src/assets/pixel-pokeball-pixel-art-pokemon-pokeball-nintendo-8bit_grande.webp"
+              alt="Placeholder Image"
+              v-else
+            />
+          </div>
         </div>
 
-        <div v-if="pokemonInfo">
-          <p>Nome: {{ pokemonInfo.name }}</p>
+        <div v-if="pokemonInfo" class="ms_poke-card">
+          <h3 class="ms_text-center">{{ firstL(pokemonInfo.name) }}</h3>
           <div v-for="pokeStat in pokemonInfo.stats" class="battle-stats">
-            <p>{{ pokeStat.stat.name }}</p> 
-            <p>{{ pokeStat.base_stat }}</p>
+            <p>{{ firstL(pokeStat.stat.name) }} : {{ pokeStat.base_stat }}</p>
             <div class="stat-bar">
-              <div class="stat-bar-fill" :style="{ width : statBar(pokeStat.base_stat)}"></div>
+              <div
+                class="stat-bar-fill"
+                :style="{ width: statBar(pokeStat.base_stat) }"
+              ></div>
             </div>
           </div>
         </div>
@@ -90,10 +86,8 @@ export default {
           <p>Inserisci un nome Pokémon per vedere le informazioni.</p>
         </div>
       </div>
-      <div
-        class="ms_pokedex-r"
-        style="width: 50%; background-color: brown; color: white"
-      >
+
+      <div class="ms_pokedex-r">
         La mia Squadra Pokémon, salvata in localStorage
       </div>
     </div>
@@ -101,21 +95,76 @@ export default {
 </template>
 
 <style>
+.ms_text-center{
+  text-align: center;
+}
+.ms_poke-card{
+  padding: 24px;
+}
 /* Contenitore della barra */
 .stat-bar {
   width: 100%;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 12px;
   overflow: hidden;
-  height: 16px;
+  height: 20px;
   transition: 0.7s all;
 }
 
 /* Parte riempita della barra */
 .stat-bar-fill {
   height: 100%;
-  background-color: #4caf50; 
+  background-color: #4caf50;
   width: 0;
   transition: width 0.5s ease;
+}
+
+.ms_poke-image {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+}
+
+.ms_poke-image-rel {
+  width: 100%; /* Imposta la larghezza del contenitore interno */
+  height: 160px; /* Limita l'altezza massima */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden; /* Nasconde le parti dell'immagine che escono dai bordi */
+}
+
+.pokemon-img,
+.placeholder-img {
+  width: 100%; /* Adatta l'immagine alla larghezza del contenitore */
+  height: 160px; /* Mantiene il rapporto di aspetto dell'immagine */
+}
+
+.container {
+  width: 100%;
+  margin-top: 120px;
+}
+
+.ms_pokedex {
+  margin: 0 auto;
+  border: 10px solid black;
+  width: 75%;
+  height: 700px;
+  display: flex;
+}
+
+.ms_pokedex-l {
+  width: 50%;
+  background-color: darkred;
+  color: white;
+  display: flex;
+  flex-direction: column;
+}
+
+.ms_pokedex-r {
+  width: 50%;
+  background-color: brown;
+  color: white;
 }
 </style>
